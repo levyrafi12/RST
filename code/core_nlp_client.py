365c7caab17ec259813b07653b6e5f037f43adda
levyrafi12 <raffilevy@gmail.com>
2019-07-24 08:40:28 +0300

    update

---
+++ code/core_nlp_client.py
@@ -0,0 +1,5 @@
+from nltk.parse.corenlp import CoreNLPDependencyParser
+
+parser = CoreNLPDependencyParser()
+parse = next(parser.raw_parse("I put the book in the box on the table."))
+print(parse)
---
+++ code/core_nlp_server.py
@@ -0,0 +1,10 @@
+from nltk.parse.corenlp import CoreNLPServer
+import os
+
+STANFORD = os.path.join("..", "stanford-corenlp-full-2018-10-05")
+
+server = CoreNLPServer(
+	os.path.join(STANFORD, "stanford-corenlp-3.9.2.jar"),
+	os.path.join(STANFORD, "stanford-corenlp-3.9.2-models.jar"))
+
+server.start()
