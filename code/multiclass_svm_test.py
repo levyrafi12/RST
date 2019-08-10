e531c7c2e485d4f0a25e902ed2cd2efcee3758e4
levyrafi18 <raffilevy@gmail.com>
2019-08-10 17:55:12 +0300

    update

---
+++ code/multiclass_svm_test.py
@@ -0,0 +1,20 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+import numpy as np
+
+from multiclass_svm import MulticlassSVM
+
+X = np.array([[0,0,0,1], [1,0,0,0], [0,1,0,0], [0,0,1,0],[0,0,1,1]])
+y = np.array([6, 0, 1, 2,2])
+
+test_x = np.array([[0,0,0,1]])
+
+clf = MulticlassSVM(C=1, tol=0.001, max_iter=500, random_state=0, verbose=0)
+clf.fit(X, y)
+
+print(clf._label_encoder.classes_)
+print(clf._label_encoder.transform([6]))
+print("predict {}".format(clf.predict(test_x)))
+print(clf.coef_)
+print(clf.dual_coef_)
