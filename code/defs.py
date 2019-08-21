e5a72e9fd27682022bc10db19754c0798e2dadd3
levyrafi12 <raffilevy@gmail.com>
2019-08-21 15:57:03 +0300

    update

--- code/defs.py
+++ code/defs.py
@@ -1,7 +1,7 @@
 # Directories variables
 WORK_DIR = "." # current working dir
-TRAINING_DIR = "..\\dataset\\TRAIN_SUBSET" # directory of the training dataset
-DEV_TEST_DIR = "..\\dataset\\DEV_SUBSET" # directory of input dev/test dataset
+TRAINING_DIR = "..\\dataset\\TRAINING" # directory of the training dataset
+DEV_TEST_DIR = "..\\dataset\\DEV" # directory of input dev/test dataset
 DEV_TEST_GOLD_DIR = "..\\work\\dev_gold" # dir of the output golden serial trees of dev/test dataset
 PRED_OUTDIR = "..\\work\\pred" # directory of the generated predicted serial trees
 GLOVE_DIR = "..\\glove" # in which the glove embedding vectors file exists (glove.6B.50d.txt)
